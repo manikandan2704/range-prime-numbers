2a020501ee334acbd2311d7e6789bbd798a92222
manikandan <manimani19508@gmail.com>
2022-11-04 15:47:45 +0530

    add new file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let num1=2;
+// let num2=23;
+
+// for (let i = num1; i <= num2; i++) {
+//     let flag = 0;
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) {
+//             flag = 1;
+//             break;
+//         }
+//     }
+//      if (i > 1 && flag == 0) {
+//         console.log(i);
+//     }
+// }
